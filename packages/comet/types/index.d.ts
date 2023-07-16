b6bc073eef2dedaf537616089069b649d7e53e03
Tony Sullivan <15836226+tony-sull@users.noreply.github.com>
2023-07-15 23:41:45 -0500

    removing built types from git

--- packages/comet/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- *
- * @returns {import("astro").AstroIntegration}
- */
-export default function createIntegration(): import("astro").AstroIntegration;
-//# sourceMappingURL=index.d.ts.map
--- packages/types/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./article";
-export * from "./bookmark";
-export * from "./media";
-export * from "./note";
-export * from "./person";
-//# sourceMappingURL=index.d.ts.map
